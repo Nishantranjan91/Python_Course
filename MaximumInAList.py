8d7700bb9fd8d9237e0316f4198e61b706882ca8
Nishant <nishantranjj91555@gmail.com>
2026-03-28 16:22:56 +0530

    MaximumInAList.py

--- MaximumInAList.py
+++ MaximumInAList.py
@@ -1,9 +1,29 @@
+# a = [1,45,23,89,45,90,12,36,82]
+# max = a[0]
+# index = 0
+
+# for i in range(len(a)):
+#     if a[i] > max:
+#         max = a[i]
+#         index = i
+# print(f"your maximum element is {max} at index {index}")      
+  
+
+
+
+
+
 a = [1,45,23,89,45,90,12,36,82]
 max = a[0]
+max2 = a[0]
 index = 0
-
+index2 = 0
 for i in range(len(a)):
     if a[i] > max:
-        max = a[i]
+        max2 = max
+        index2 = index
         index = i
-print(f"your maximum element is {max} at index {index}")        
+    elif a[i] > max2:
+        max2 = a[i]
+        index2 = i    
+print(f"max {max}at {index} and max2 is {max2} at {index2}")        
