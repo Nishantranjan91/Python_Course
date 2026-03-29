d37a52b3b883f1ce3ac4228a743d2b2eb4b7096b
Nishant <nishantranjj91555@gmail.com>
2026-03-29 10:32:04 +0530

    tuple.py

--- tuple.py
+++ tuple.py
@@ -1,4 +1,12 @@
-a = (12,12.4,"hello",print())
+#tuple unpacking
+# a = (12,12.4,"hello",print())
 
-a, b, c, d, e = (1,2,3,4,5)
-print(b)
+# a, b, c, d, e = (1,2,3,4,5)
+# print(b)
+
+
+
+
+
+a = (1)
+print(type(a))
