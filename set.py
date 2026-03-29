b812be7db0cdf357a4b55402f303d84a13d7aefd
Nishant <nishantranjj91555@gmail.com>
2026-03-29 11:40:39 +0530

    set.py

--- set.py
+++ set.py
@@ -4,5 +4,17 @@
 
 
 
-s = {10,20,30,40,50}
-print(hash("hello"))
+#hash value
+# s = {10,20,30,40,50}
+# print(hash("hello"))
+
+
+
+
+
+
+
+# set difference
+s1 = {10,20,30,40,50,60}
+s2 = {40,50,60,70,10,90}
+print(s1.difference(s2))
