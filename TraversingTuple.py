9bef03b77b041dc2b991864ef08d41ca55687d0f
Nishant <nishantranjj91555@gmail.com>
2026-03-29 10:55:23 +0530

    TraversingTuple.py

--- TraversingTuple.py
+++ TraversingTuple.py
@@ -7,6 +7,15 @@
 
 
 #second way
-t = (10,20,30,40,50)
-for i in range(len(t)):
-    print(t[i])
+# t = (10,20,30,40,50)
+# for i in range(len(t)):
+#     print(t[i])
+
+
+
+
+
+
+a = (10,20,10,10,20,30,40,40,50)
+print(a.count(40))
+print(a.index(30))
