e30899e2155c2327c41bda0ca3904689dfa7ea1a
Nishant <nishantranjj91555@gmail.com>
2026-03-30 12:11:08 +0530

    oops.py

--- oops.py
+++ oops.py
@@ -1,6 +1,23 @@
 #class and object
+# class Factory:
+#     a = 12  #attribute
+#     def hello(): 
+#         print("how are you")
+# print(Factory.a)   
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# class
 class Factory:
-    a = 12  #attribute
-    def hello(): 
-        print("how are you")
-print(Factory.a)         
+    a = "hello I am attribute"      
+    def hello():
+        print("hello I am a method")
+
+        obj = Factory( ) # obj now becomes a object who can access anything inside the till now
+        obj2 = Factory()
+        print(obj.a)
+        obj2.hello()
