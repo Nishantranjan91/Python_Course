608996deadc3b251e90358e91d13a96a2dfab77f
Nishant <nishantranjj91555@gmail.com>
2026-03-30 15:33:48 +0530

    attribute&method.py

---
+++ attribute&method.py
@@ -0,0 +1,10 @@
+class Animal:
+    gender = "Male"
+    def __init__(self, name,age):
+        self.name = name 
+        self.age = age
+        pass
+    
+    def speak():
+        print("lion is roaring")
+object = Animal("lion",12)
--- oops.py
+++ oops.py
@@ -12,12 +12,37 @@
 # 
 # 
 # class
-class Factory:
-    a = "hello I am attribute"      
-    def hello():
-        print("hello I am a method")
+# class Factory:
+#     a = "hello I am attribute"      
+#     def hello():
+#         print("hello I am a method")
 
-        obj = Factory( ) # obj now becomes a object who can access anything inside the till now
-        obj2 = Factory()
-        print(obj.a)
-        obj2.hello()
+#         obj = Factory( ) # obj now becomes a object who can access anything inside the till now
+#         obj2 = Factory()
+#         print(obj.a)
+        # obj2.hello()
+
+
+
+
+
+# creating a constructor
+# def Factory(material, zips, pockets):
+#     def __init__() :
+#         print("hello I will run no matter what")
+# Factory()        
+
+
+
+
+
+
+class Factory:
+    def __init__(self, material, zips, pockets):
+        
+        self.material = material
+        self.zips = zips
+        self.pockets = pockets
+obj = Factory("Leater",3,3)        
+        
+
