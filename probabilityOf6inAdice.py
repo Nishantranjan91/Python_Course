bc76cd610cc922280bfec9e2320ef5abab274088
Nishant <nishantranjj91555@gmail.com>
2026-04-02 07:50:25 +0530

    probabilityOf6inAdice.py

---
+++ probabilityOf6inAdice.py
@@ -0,0 +1,10 @@
+# Total possible outcomes when a dice is thrown
+total_outcomes = 6
+
+# Favorable outcomes (only one face has 4)
+favorable_outcomes = 1
+
+# Probability formula
+probability = favorable_outcomes / total_outcomes
+
+print("Probability of getting 4 in one throw of a dice is:", probability)
