fa6e082d7f0464ed08237bf063219693c03bef8d
Nishant <nishantranjj91555@gmail.com>
2026-04-08 19:47:03 +0530

    multipleOf15.py

---
+++ multipleOf15.py
@@ -0,0 +1,8 @@
+# Program to check multiple of 15
+
+num = int(input("Enter a number: "))
+
+if num % 15 == 0:
+    print("The number is a multiple of 15")
+else:
+    print("The number is NOT a multiple of 15")
