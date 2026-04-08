2c657160ab1c78934836c7af6f2a27376d603640
Nishant <nishantranjj91555@gmail.com>
2026-04-08 19:59:28 +0530

    multipleOf19.py

---
+++ multipleOf19.py
@@ -0,0 +1,8 @@
+# Program to check multiple of 19
+
+num = int(input("Enter a number: "))
+
+if num % 19 == 0:
+    print("The number is a multiple of 19")
+else:
+    print("The number is NOT a multiple of 19")
