f747c70bb71664802b4f4189fa98a67f6d9af43f
Nishant <nishantranjj91555@gmail.com>
2026-04-11 11:28:21 +0530

    Project_numpy_2.py

--- Project_numpy_2.py
+++ Project_numpy_2.py
@@ -3,3 +3,13 @@ import numpy as np
 board = np.zeros((3,3),dtype=int)
 
 print(board)
+
+def print_board(b):
+    symbols = {0: " ", 1: "x", -1: "0"}
+    for r in range(3):
+        row = "  | ".join(symbols[val] for val in b[r])
+        print(" "+ row)
+        if r < 2:
+            print("---+---+---")
+            print()
+print_board(board)            
