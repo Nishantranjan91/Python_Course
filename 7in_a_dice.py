57661fe17d7a447451ece89d3c8df659064300b1
Nishant <nishantranjj91555@gmail.com>
2026-04-14 21:59:30 +0530

    7in_a_dice.py

---
+++ 7in_a_dice.py
@@ -0,0 +1,8 @@
+# Probability of getting 7 on a single die
+
+total_outcomes = 6   # numbers 1 to 6
+favorable_outcomes = 0  # no way to get 7
+
+probability = favorable_outcomes / total_outcomes
+
+print("Probability of getting 7:", probability)
