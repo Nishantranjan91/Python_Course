556c6272524536cdfb845ea82d805f23b50195cb
Nishant <nishantranjj91555@gmail.com>
2026-04-27 21:49:10 +0530

    spaces_in_a_paragraph.py

---
+++ spaces_in_a_paragraph.py
@@ -0,0 +1,8 @@
+# Take input from the user
+paragraph = input("Enter a paragraph: ")
+
+# Count spaces
+space_count = paragraph.count(' ')
+
+# Display result
+print("Number of spaces:", space_count)
