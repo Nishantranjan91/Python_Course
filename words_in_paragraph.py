a9225ecf170d81a03fd3ffdae14811e7c936d5a6
Nishant <nishantranjj91555@gmail.com>
2026-04-27 21:42:47 +0530

    words_in_paragraph.py

---
+++ words_in_paragraph.py
@@ -0,0 +1,11 @@
+# Take input from the user
+paragraph = input("Enter a paragraph: ")
+
+# Split the paragraph into words
+words = paragraph.split()
+
+# Count the number of words
+word_count = len(words)
+
+# Display the result
+print("Number of words:", word_count)
