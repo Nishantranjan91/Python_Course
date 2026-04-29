7abc7ebbad18d2846f084a04672ff28ac3234168
Nishant <nishantranjj91555@gmail.com>
2026-04-29 21:48:49 +0530

    e_to_the_power_x.py

---
+++ e_to_the_power_x.py
@@ -0,0 +1,9 @@
+import math
+
+a = float(input("Enter value of a: "))
+b = float(input("Enter value of b: "))
+x = float(input("Enter value of x: "))
+
+y = a * math.exp(b * x)
+
+print("Result:", y)
