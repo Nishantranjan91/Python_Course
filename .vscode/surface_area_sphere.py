8851584ccdce0b67a4b80f841960d4c069596420
Nishant <nishantranjj91555@gmail.com>
2026-05-07 23:08:36 +0530

    surface_area_sphere.py

---
+++ .vscode/surface_area_sphere.py
@@ -0,0 +1,10 @@
+import math
+
+# Input radius from user
+radius = float(input("Enter the radius of the sphere: "))
+
+# Calculate surface area of sphere
+area = 4 * math.pi * radius ** 2
+
+# Display result
+print("Surface Area of the Sphere =", area)
