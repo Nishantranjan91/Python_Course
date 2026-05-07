6a53d73c43637d1f64f46898d786da050f23681f
Nishant <nishantranjj91555@gmail.com>
2026-05-07 23:10:10 +0530

    volume_of_sphere.py

---
+++ .vscode/volume_of_sphere.py
@@ -0,0 +1,10 @@
+import math
+
+# Input radius from user
+radius = float(input("Enter the radius of the sphere: "))
+
+# Calculate volume of sphere
+volume = (4/3) * math.pi * radius ** 3
+
+# Display result
+print("Volume of the Sphere =", volume)
