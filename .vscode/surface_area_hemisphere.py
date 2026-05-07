58ac41c8feb4cd354a3bff4beff77c3b2485c750
Nishant <nishantranjj91555@gmail.com>
2026-05-07 23:12:14 +0530

    surface_area_hemisphere.py

---
+++ .vscode/surface_area_hemisphere.py
@@ -0,0 +1,10 @@
+import math
+
+# Input radius from user
+radius = float(input("Enter the radius of the hemisphere: "))
+
+# Calculate total surface area of hemisphere
+tsa = 3 * math.pi * radius ** 2
+
+# Display result
+print("Total Surface Area of the Hemisphere =", tsa)
