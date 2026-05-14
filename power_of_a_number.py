7f59accbe4d33870463c78bab873a9c58912fe5c
Nishant <nishantranjj91555@gmail.com>
2026-05-14 23:37:11 +0530

    power_of_a_number.py

---
+++ power_of_a_number.py
@@ -0,0 +1,8 @@
+# Python program to find the power of a number
+
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+power = base ** exponent
+
+print("Power =", power)
