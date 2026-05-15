8cfa08f2ac62e730436459266502e595e08cd0c9
Nishant <nishantranjj91555@gmail.com>
2026-05-15 23:00:28 +0530

    prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,19 @@
+# Python program to find prime numbers
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    is_prime = True
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, "is a Prime Number")
+    else:
+        print(num, "is not a Prime Number")
+
+else:
+    print(num, "is not a Prime Number")
